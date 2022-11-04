c4d835353fd227745d439214642dbfe86d2513eb
JorVLP <47005135+JorVLP@users.noreply.github.com>
2022-11-04 17:32:16 +0000

    add abstract base class for TargetSelectors

--- sapai/effect/targets/target_selectors.py
+++ sapai/effect/targets/target_selectors.py
@@ -1,2 +1,14 @@
-class TargetSelector:
+from abc import ABC, abstractmethod
+
+from sapai.pets import Pet
+
+
+class TargetSelector(ABC):
     """Selects a target(s) from a list of possible targets"""
+
+    @abstractmethod
+    def select(pets: list[Pet], n: int) -> list[Pet]:
+        pass
+
+
+# class RandomTargetSelector(TargetSelector):
