8a8d7ccc416f358d5955a7a7b4267d09605b3846
JorVLP <47005135+JorVLP@users.noreply.github.com>
2022-11-05 22:30:54 +0000

    permutations

--- sapai/effect/targets/target_selectors.py
+++ sapai/effect/targets/target_selectors.py
@@ -15,6 +15,8 @@ class FirstTargetSelector(TargetSelector):
     """Selects the first n targets"""
 
     def select(self, pets: list[Pet], n: int, seed: int = None) -> list[Pet]:
+        if n <= 0:
+            return []
         return pets[:n]
 
 
@@ -22,4 +24,36 @@ class LastTargetSelector(TargetSelector):
     """Selects the last n targets"""
 
     def select(self, pets: list[Pet], n: int, seed: int = None) -> list[Pet]:
+        if n <= 0:
+            return []
         return pets[-n:]
+
+
+class RandomTargetSelector(TargetSelector):
+    """Selects n random pets from a list"""
+
+    def select(self, pets: list[Pet], n: int, seed: int = None) -> list[Pet]:
+        pass
+
+
+def nth_permutation(items: list, n: int, size: int):
+    """Returns the nth permutation of the given size from the list of items.
+
+    For example in the list [1,2,3], the first permutation of size 2 would be
+    [1,2], the second would be [2,1]
+
+    Args:
+        items (list): _description_
+        n (int): _description_
+        size (int): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    result = []
+    for _ in range(size):
+        item = n % len(items)
+        n = n // len(items)
+        result.append(items.pop(item))
+
+    return result
--- tests/effect/targets/test_target_selectors.py
+++ tests/effect/targets/test_target_selectors.py
@@ -1,7 +1,11 @@
 from unittest import TestCase
 from unittest.mock import Mock
 from sapai.effect.events import Event, EventType
-from sapai.effect.targets import FirstTargetSelector, LastTargetSelector
+from sapai.effect.targets import (
+    FirstTargetSelector,
+    LastTargetSelector,
+    nth_permutation,
+)
 from sapai.pets import Pet
 
 
@@ -11,11 +15,36 @@ class TargetSelectorTestCase(TestCase):
         self.enemy_team = [Mock(Pet) for i in range(5)]
 
     def test_first_target_selector(self):
-        """Tests AllTargetPool returns all friendly and enemy pets"""
+        """Tests first target selector returns first n pets"""
         selector = FirstTargetSelector()
+        self.assertEqual([], selector.select(self.friendly_team, n=-1))
         self.assertEqual([], selector.select(self.friendly_team, n=0))
         self.assertEqual(
             [self.friendly_team[0]], selector.select(self.friendly_team, n=1)
         )
+        self.assertEqual(
+            [self.friendly_team[0], self.friendly_team[1]],
+            selector.select(self.friendly_team, n=2),
+        )
         self.assertEqual(self.friendly_team, selector.select(self.friendly_team, n=5))
         self.assertEqual(self.friendly_team, selector.select(self.friendly_team, n=10))
+
+    def test_last_target_selector(self):
+        """Tests last target selector returns last n pets"""
+        selector = LastTargetSelector()
+        self.assertEqual([], selector.select(self.friendly_team, n=-1))
+        self.assertEqual([], selector.select(self.friendly_team, n=0))
+        self.assertEqual(
+            [self.friendly_team[-1]], selector.select(self.friendly_team, n=1)
+        )
+        self.assertEqual(
+            [self.friendly_team[-2], self.friendly_team[-1]],
+            selector.select(self.friendly_team, n=2),
+        )
+        self.assertEqual(self.friendly_team, selector.select(self.friendly_team, n=5))
+        self.assertEqual(self.friendly_team, selector.select(self.friendly_team, n=10))
+
+    def test_nth_permutation(self):
+        print()
+        for i in range(6):
+            print(nth_permutation(list("abcd"), i, 2))
