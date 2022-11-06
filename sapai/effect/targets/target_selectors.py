c09bf607b75f264536cc863615691b0757883bba
JorVLP <47005135+JorVLP@users.noreply.github.com>
2022-11-06 17:51:55 +0000

    add random target selector

--- sapai/effect/targets/target_selectors.py
+++ sapai/effect/targets/target_selectors.py
@@ -1,30 +1,49 @@
 from abc import ABC, abstractmethod
+import math
 
 from sapai.pets import Pet
+from sapai.effect.utils import nth_combination
 
 
 class TargetSelector(ABC):
     """Selects a target(s) from a list of possible targets"""
 
+    def _validate_args(self, pets, n, rand):
+        if n < 0:
+            raise ValueError("number of selected pets must be > 0")
+        if rand is not None and (rand < 0 or rand >= 1):
+            raise ValueError("rand must be in the range [0,1)")
+
     @abstractmethod
-    def select(self, pets: list[Pet], n: int, seed: int = None) -> list[Pet]:
+    def select(self, pets: list[Pet], n: int, rand: float) -> list[Pet]:
+        """Select n items from the given list of pets
+
+        Args:
+            pets (list[Pet]): List of pets to choose from
+            n (int): Number of pets to select
+            rand (int): Number to determine random effects.
+                Must follow `0 >= rand and rand < 1`.
+
+        Returns:
+            list[Pet]: _description_
+        """
         pass
 
 
 class FirstTargetSelector(TargetSelector):
     """Selects the first n targets"""
 
-    def select(self, pets: list[Pet], n: int, seed: int = None) -> list[Pet]:
-        if n <= 0:
-            return []
+    def select(self, pets: list[Pet], n: int, rand: float = None) -> list[Pet]:
+        self._validate_args(pets, n, rand)
         return pets[:n]
 
 
 class LastTargetSelector(TargetSelector):
     """Selects the last n targets"""
 
-    def select(self, pets: list[Pet], n: int, seed: int = None) -> list[Pet]:
-        if n <= 0:
+    def select(self, pets: list[Pet], n: int, rand: float = None) -> list[Pet]:
+        self._validate_args(pets, n, rand)
+        if n == 0:
             return []
         return pets[-n:]
 
@@ -32,5 +51,11 @@ class LastTargetSelector(TargetSelector):
 class RandomTargetSelector(TargetSelector):
     """Selects n random pets from a list"""
 
-    def select(self, pets: list[Pet], n: int, seed: int = None) -> list[Pet]:
-        pass
+    def select(self, pets: list[Pet], n: int, rand: float) -> list[Pet]:
+        """Selects the combination at the index: floor(rand * (len(pets) Choose n))"""
+        self._validate_args(pets, n, rand)
+        n = min(n, len(pets))
+        comb = math.comb(len(pets), n)
+        index = math.floor(rand * comb)
+        result = nth_combination(pets, n, index)
+        return list(result)
--- tests/effect/targets/test_target_selectors.py
+++ tests/effect/targets/test_target_selectors.py
@@ -1,7 +1,12 @@
 from unittest import TestCase
 from unittest.mock import Mock
 from sapai.effect.events import Event, EventType
-from sapai.effect.targets import FirstTargetSelector, LastTargetSelector
+from sapai.effect.targets import (
+    FirstTargetSelector,
+    LastTargetSelector,
+    RandomTargetSelector,
+    TargetSelector,
+)
 from sapai.pets import Pet
 
 
@@ -10,10 +15,18 @@ class TargetSelectorTestCase(TestCase):
         self.friendly_team = [Mock(Pet) for i in range(5)]
         self.enemy_team = [Mock(Pet) for i in range(5)]
 
+    def test_validate_args(self):
+        selector = Mock(TargetSelector)
+        with self.assertRaises(ValueError):
+            TargetSelector._validate_args(selector, pets=[], n=-1, rand=0)
+        with self.assertRaises(ValueError):
+            TargetSelector._validate_args(selector, pets=[], n=1, rand=-0.01)
+        with self.assertRaises(ValueError):
+            TargetSelector._validate_args(selector, pets=[], n=1, rand=1)
+
     def test_first_target_selector(self):
         """Tests first target selector returns first n pets"""
         selector = FirstTargetSelector()
-        self.assertEqual([], selector.select(self.friendly_team, n=-1))
         self.assertEqual([], selector.select(self.friendly_team, n=0))
         self.assertEqual(
             [self.friendly_team[0]], selector.select(self.friendly_team, n=1)
@@ -28,7 +41,6 @@ class TargetSelectorTestCase(TestCase):
     def test_last_target_selector(self):
         """Tests last target selector returns last n pets"""
         selector = LastTargetSelector()
-        self.assertEqual([], selector.select(self.friendly_team, n=-1))
         self.assertEqual([], selector.select(self.friendly_team, n=0))
         self.assertEqual(
             [self.friendly_team[-1]], selector.select(self.friendly_team, n=1)
@@ -39,3 +51,17 @@ class TargetSelectorTestCase(TestCase):
         )
         self.assertEqual(self.friendly_team, selector.select(self.friendly_team, n=5))
         self.assertEqual(self.friendly_team, selector.select(self.friendly_team, n=10))
+
+    def test_random_target_selector(self):
+        selector = RandomTargetSelector()
+        self.assertEqual([], selector.select(self.friendly_team, n=0, rand=0))
+        # Since we use the seed for the index of the nth combination,
+        # the random pet chosen is consistent with its index in the list
+        n = len(self.friendly_team)
+        for i, p in enumerate(self.friendly_team):
+            self.assertEqual([p], selector.select(self.friendly_team, n=1, rand=i / n))
+
+        self.assertEqual(
+            self.friendly_team[:2],
+            selector.select(self.friendly_team, n=2, rand=0),
+        )
