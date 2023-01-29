169b18d5ee7608dcf6d55eb82bd98618aaef873a
JorVLP <47005135+JorVLP@users.noreply.github.com>
2023-01-29 21:08:12 +0000

    test add_stats

--- sapai/rewrite/pets.py
+++ sapai/rewrite/pets.py
@@ -4,18 +4,25 @@
 class Pet:
     """Pet class"""
 
-    def __init__(self) -> None:
-        self.name = ""
+    STAT_CAP = 50
+
+    def __init__(self, name="", stats: tuple = None) -> None:
+        self.name = name
         self.tier = 1
 
         self._perm_attack = 1
         self._perm_health = 1
+        self._temp_attack = 0
+        self._temp_health = 0
+        if stats:
+            if len(stats) == 2 or len(stats) == 4:
+                self._perm_attack, self._perm_health = stats[:2]
+            if len(stats) == 4:
+                self._temp_attack, self._temp_health = stats[2:]
 
         self.ability = None
         self.perk = None
 
-        self._temp_attack = 0
-        self._temp_health = 0
         self.experience = 0
         self.level = 1
 
@@ -35,9 +42,22 @@ class Pet:
             health (int, optional): Health to add
             temp_stats (bool, optional): True if stats should be temporary until end of battle
         """
+        # Ensure total stats don't go over STAT_CAP or under 1
+        attack_added = min(self.STAT_CAP - self.attack, max(1 - self.attack, attack))
+        health_added = min(self.STAT_CAP - self.health, max(1 - self.health, health))
         if temp_stats:
-            self._temp_attack += attack
-            self._temp_health += health
+            self._temp_attack += attack_added
+            self._temp_health += health_added
         else:
-            self._perm_attack += attack
-            self._perm_health += health
+            # Logic to convert temp stats into perm stats
+            if self.attack + attack >= self.STAT_CAP:
+                attack_diff = attack - attack_added
+                self._temp_attack -= attack_diff
+                attack_added += attack_diff
+            if self.health + health >= self.STAT_CAP:
+                health_diff = health - health_added
+                self._temp_health -= health_diff
+                health_added += health_diff
+
+            self._perm_attack += attack_added
+            self._perm_health += health_added
---
+++ tests/rewrite/test_pets.py
@@ -0,0 +1,111 @@
+import pytest
+
+from sapai.rewrite.pets import Pet
+
+
+@pytest.mark.parametrize(
+    ["pet", "attack", "health", "temp_stats", "exp_stats"],
+    [
+        (Pet(stats=(1, 1)), 0, 0, False, (1, 1)),
+        (Pet(stats=(1, 1)), 1, 1, False, (2, 2)),
+        (Pet(stats=(1, 1)), 1, 1, True, (2, 2)),
+        (Pet(stats=(1, 1)), 9, 1, True, (10, 2)),
+        (Pet(stats=(1, 1)), 1, 9, False, (2, 10)),
+        (Pet(stats=(5, 7)), 9, 0, True, (14, 7)),
+        (Pet(stats=(5, 7)), 0, 9, False, (5, 16)),
+        (Pet(stats=(10, 10)), -5, -9, False, (5, 1)),
+        (Pet(stats=(10, 10)), -9, -5, True, (1, 5)),
+    ],
+)
+def test_pet_add_stats(pet: Pet, attack: int, health: int, temp_stats: bool, exp_stats):
+    """Adding / removing stats within max/mins"""
+    base_atk, base_hea = pet._perm_attack, pet._perm_health
+    base_tatk, base_thea = pet._temp_attack, pet._temp_health
+    pet.add_stats(attack, health, temp_stats)
+    # Stats increase as expected
+    assert (pet.attack, pet.health) == exp_stats
+    assert pet.attack == base_atk + base_tatk + attack
+    assert pet.health == base_hea + base_thea + health
+    # Perm / temp stats only changed when supposed to
+    if temp_stats:
+        assert (base_atk, base_hea) == (pet._perm_attack, pet._perm_health)
+        assert (pet._temp_attack, pet._temp_health) == (
+            attack + base_tatk,
+            health + base_thea,
+        )
+    else:
+        assert (base_tatk, base_thea) == (pet._temp_attack, pet._temp_health)
+        assert (pet._perm_attack, pet._perm_health) == (
+            attack + base_atk,
+            health + base_hea,
+        )
+
+
+@pytest.mark.parametrize(
+    ["pet", "is_temp", "stats"],
+    [
+        (Pet(stats=(1, 1)), False, (-1, -1)),
+        (Pet(stats=(1, 1)), True, (-1, -1)),
+        (Pet(stats=(5, 4)), False, (-20, -9)),
+        (Pet(stats=(5, 4)), True, (-20, -9)),
+        (Pet(stats=(10, 10, -9, -9)), False, (-3, -3)),
+        (Pet(stats=(10, 10, -9, -9)), True, (-3, -3)),
+    ],
+)
+def test_pet_add_stats_min(pet, is_temp, stats):
+    """pet total stats never go below 1"""
+    pet.add_stats(*stats, is_temp)
+    assert pet.attack == 1 and pet.health == 1
+    # Permanent stats always above 1
+    assert pet._perm_attack >= 1 and pet._perm_health >= 1
+
+
+@pytest.mark.parametrize(
+    ["pet", "is_temp"],
+    [
+        (Pet(stats=(3, 3)), False),
+        (Pet(stats=(3, 3)), True),
+        (Pet(stats=(15, 27)), False),
+        (Pet(stats=(15, 27)), True),
+    ],
+)
+def test_pet_add_stats_temp_min_with_add(pet, is_temp):
+    """add stats after reducing to minimum stats"""
+    is_temp = False
+    pet = Pet(stats=(3, 3))
+    pet.add_stats(-50, -50, is_temp)
+    pet.add_stats(1, 3)
+    assert pet.attack == 2 and pet.health == 4
+
+
+@pytest.mark.parametrize(
+    ["pet", "is_temp", "stats"],
+    [
+        (Pet(stats=(1, 1)), False, (50, 50)),
+        (Pet(stats=(1, 1)), True, (50, 50)),
+        (Pet(stats=(1, 1, 10, 10)), False, (50, 50)),
+        (Pet(stats=(1, 1, 10, 10)), True, (50, 50)),
+    ],
+)
+def test_pet_add_stats_max(pet, is_temp, stats):
+    """attack/health both stay below STAT_CAP"""
+    pet.add_stats(*stats, is_temp)
+    assert pet.attack == Pet.STAT_CAP and pet.health == Pet.STAT_CAP
+
+
+@pytest.mark.parametrize(
+    ["pet", "stats", "is_temp", "exp_stats"],
+    [
+        (Pet(stats=(25, 25, 25, 25)), (10, 12), False, (35, 37, 15, 13)),
+        (Pet(stats=(25, 25, 25, 25)), (10, 12), True, (25, 25, 25, 25)),
+        (Pet(stats=(25, 10, 15, 10)), (20, 20), False, (45, 30, 5, 10)),
+        (Pet(stats=(10, 10, 40, 40)), (20, 00), False, (30, 10, 20, 40)),
+    ],
+)
+def test_pet_add_stats_max_temp_to_perm(pet, stats, is_temp, exp_stats):
+    """Adding perm stats should override temp stats when the stat is at STAT_CAP"""
+    pet.add_stats(*stats, is_temp)
+    assert pet.attack <= Pet.STAT_CAP and pet.health <= Pet.STAT_CAP
+
+    assert pet._perm_attack == exp_stats[0] and pet._perm_health == exp_stats[1]
+    assert pet._temp_attack == exp_stats[2] and pet._temp_health == exp_stats[3]
