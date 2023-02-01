9fda8e0d6334fab29f6c77dc9be7cf45af140047
JorVLP <47005135+JorVLP@users.noreply.github.com>
2023-02-01 23:05:40 +0000

    start summon pet

--- superautosim/teams.py
+++ superautosim/teams.py
@@ -20,5 +20,16 @@ class Team:
         empty_slots = self.MAX_TEAM_SIZE - len(pets)
         self._slots: list[Pet | None] = list(pets) + [None] * empty_slots
 
+    def summon_pet(self, summoned_idx: int, pet: Pet):
+        """Summon a pet as close as possible to the given idx
+
+        Args:
+            summoned_idx (int): _description_
+            pet (Pet): _description_
+        """
+
     def __iter__(self):
         return iter(self._slots)
+
+    def __repr__(self) -> str:
+        return f"Team<{self._slots}>"
