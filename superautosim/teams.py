79822b4c2fa9b11313a48c99c34fbccb00271215
JorVLP <47005135+JorVLP@users.noreply.github.com>
2023-02-13 23:56:23 +0000

    implement insert_pet

--- superautosim/teams.py
+++ superautosim/teams.py
@@ -11,22 +11,60 @@ class Team:
 
     MAX_TEAM_SIZE = 5
 
-    def __init__(self, pets: Sequence[Pet | None] = None) -> None:
+    def __init__(self, pets: Sequence[Pet | None] | None = None) -> None:
         if pets is None:
             pets = []
         if len(pets) > self.MAX_TEAM_SIZE:
             raise ValueError("Team pets must be less than MAX_TEAM_SIZE")
 
         empty_slots = self.MAX_TEAM_SIZE - len(pets)
+        # Slots always has a length of MAX_TEAM_SIZE
         self._slots: list[Pet | None] = list(pets) + [None] * empty_slots
 
-    def summon_pet(self, summoned_idx: int, pet: Pet):
+    def summon_pet(self, pet: Pet, target_idx: int) -> bool:
         """Summon a pet as close as possible to the given idx
 
         Args:
-            summoned_idx (int): _description_
-            pet (Pet): _description_
+            pet (Pet): Pet to summon
+            target_idx (int): Index of slot to attempt the summon.
+
+        Returns:
+            bool: True when the pet was successfully summoned.
         """
+        if 0 > target_idx < self.MAX_TEAM_SIZE:
+            raise IndexError("Index to summon pet is out of bounds")
+        if None in self:
+            return False
+
+    def insert_pet(self, pet: Pet, index: int) -> bool:
+        """Insert the pet at the given index. Returns False if there is no space.
+
+        If the team is full, return False. If a pet is already at the index, move it
+        towards the back of the team to make space, if not possible then move it forwards.
+
+        Args:
+            pet (Pet): Pet to insert.
+            index (int): Index to insert the pet
+
+        Returns:
+            bool: True if pet is successfully inserted. False otherwise.
+        """
+        if len(self.pets) >= self.MAX_TEAM_SIZE:
+            return False
+
+        # Get the index of the first None value after the index, otherwise first None infront.
+        if None in self._slots[index:]:
+            index_to_remove = self._slots.index(None, index)
+        else:
+            index_to_remove = self.MAX_TEAM_SIZE - self._slots[::-1].index(None) - 1
+        self._slots.pop(index_to_remove)
+
+        self._slots.insert(index, pet)
+        return True
+
+    @property
+    def pets(self) -> list[Pet]:
+        return [p for p in self._slots if p]
 
     def __iter__(self):
         return iter(self._slots)
--- tests/test_teams.py
+++ tests/test_teams.py
@@ -32,3 +32,34 @@ def test_team_iteration(team: Team):
 def test_team_init_validation(pets, error):
     with pytest.raises(error):
         Team(pets=pets)
+
+
+def test_team_sumon_pet_invalid_index(friendly_team: Team, pet):
+    with pytest.raises(IndexError):
+        friendly_team.summon_pet(pet, -1)
+    with pytest.raises(IndexError):
+        friendly_team.summon_pet(friendly_team.MAX_TEAM_SIZE, -1)
+
+
+@pytest.mark.parametrize(
+    ["slots", "index", "expected"],
+    [
+        ((1, 2, None, 3, 4), 4, [1, 2, 3, 4, 100]),
+        ((1, 2, 3, 4, None), 3, [1, 2, 3, 100, 4]),
+        ((1, 2, None, 3, 4), 2, [1, 2, 100, 3, 4]),
+        ((None, 1, 2, 3, 4), 1, [1, 100, 2, 3, 4]),
+        ((1, 2, None, 3, 4), 0, [100, 1, 2, 3, 4]),
+        ((None, 1, 2, 3, 4), 0, [100, 1, 2, 3, 4]),
+        ((None, 1, 2, 3, 4), 4, [1, 2, 3, 4, 100]),
+        ((1, None, 2, 3, None), 2, [1, None, 100, 2, 3]),
+        ((1, 2, 3, None, None), 2, [1, 2, 100, 3, None]),
+        ((None, None, None, None, None), 2, [None, None, 100, None, None]),
+        ((None, None, 1, None, None), 1, [None, 100, 1, None, None]),
+        ((None, 1, None, 1, None), 1, [None, 100, 1, 1, None]),
+        ((None, 1, None, 2, 3), 3, [None, 1, 2, 100, 3]),
+    ],
+)
+def test_team_insert_pet(slots, index, expected):
+    team = Team(slots)
+    assert team.insert_pet(100, index)
+    assert team._slots == expected
